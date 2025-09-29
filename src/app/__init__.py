226483e634978904e960103a35bb302645e196de
= <=>
2025-09-29 10:18:04 +0530

    added kafka

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify
 from service.messageService import MessageService
 from kafka import KafkaProducer
+import json
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
-# producer = KafkaProducer(boo)
+producer = KafkaProducer(bootstrap_servers='localhost:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 @app.route('/v1/ds/message', methods=['POST'])
 def handle_message():
     message = request.json.get('message')
     result = messageService.process_message(message)
+    producer.send('expense_service',result)
     return jsonify(result)
 
 @app.route('/', methods=['GET'])
